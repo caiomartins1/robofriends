df7ba27129f62bf338a521755b6ab5cac077621b
caiomartins1 <dev.caiomartins@gmai.com>
2020-04-22 01:21:12 +0100

    Updates

---
+++ precache-manifest.ce2f7fcb8f0f5016b47312b0e798fc65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076571df5c8e5915f7c91afe721f0f27",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "7eaac50058c351be7092",
+    "url": "/robofriends/static/css/main.2ecedc26.chunk.css"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eaac50058c351be7092",
+    "url": "/robofriends/static/js/main.039495f1.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
